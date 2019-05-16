8bf59e48a700961b8ee56ae582a85e676ababab3
arongyenes-srg <aron.gyenes@srg.hu>
2019-05-16 16:29:38 +0200

    field settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  questions="pet";
+  defaultEmail="valaki@123.com";
+  genders=["male", "female"];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+
+    });
+  }
+
+  onSubmit() {
+    this.user.username = this.signupForm.value.username;
   }
 }
